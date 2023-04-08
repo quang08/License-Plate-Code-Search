69c233907dd164604690309455fedaebe94112e2
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-08 22:12:05 +0700

    fixed placeholder message

--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ function Input({input, setInput}) {
   return (
     <div className="flex border-b border-slate-400 p-4">
       <input
-        placeholder="Search a liscene plate region code ..."
+        placeholder="Search a liscene plate region code or a city name ..."
         className="grow bg-transparent border-none outline-none h-12"
         type="text"
         value={input}
